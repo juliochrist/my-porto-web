2d31df255829c7fcd763a53b268fb3e18fa42bf7
Julio Christianto <juliochristianto@gmail.com>
2025-12-31 21:46:21 +0700

    feat: add Services section

--- src/data/services.js
+++ src/data/services.js
@@ -1,42 +1,42 @@
 export const services = [
   {
     id: 1,
-    icons: "Layout",
+    icon: "Layout",
     title: "Frontend Development",
     description:
       "Building responsive and visually appealing user interfaces using modern frontend technologies.",
   },
   {
     id: 2,
-    icons: "Smartphone",
+    icon: "Smartphone",
     title: "Responsive Design",
     description:
       "Creating designs that adapt seamlessly to various screen sizes and devices for optimal user experience.",
   },
   {
     id: 3,
-    icons: "Palette",
+    icon: "Palette",
     title: "UI/UX Design",
     description:
       "Designing intuitive and engaging user interfaces with a focus on usability and aesthetics.",
   },
   {
     id: 4,
-    icons: "Code2",
+    icon: "Code2",
     title: "Custom Component",
     description:
       "Developing reusable and customizable UI components to enhance application functionality and maintainability.",
   },
   {
     id: 5,
-    icons: "Zap",
+    icon: "Zap",
     title: "Performance Optimization",
     description:
       "Improving application speed and efficiency through code optimization and best practices.",
   },
   {
     id: 6,
-    icons: "GitBranch",
+    icon: "GitBranch",
     title: "Code Review & Consulting",
     description:
       "Providing expert code reviews and consulting services to ensure code quality and adherence to best practices.",
